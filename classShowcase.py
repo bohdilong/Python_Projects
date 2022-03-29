25adbc0eda169f10db6b69033606e6f7b4e72d56
bohdilong <98560374+bohdilong@users.noreply.github.com>
2022-03-29 12:21:38 -0700

    add comments

--- classShowcase.py
+++ classShowcase.py
@@ -1,13 +1,18 @@
+# parent class
 class User:
     name = 'no name provided'
     email = ' '
     password = 'password'
     accountNumber = 0
 
+
+# child class employee
 class Employee(User):
     basePay = 25.00
     department = 'general'
 
+
+# child class customer
 class Customer(User):
     mailingAdderss = ' '
     mailingList = True
