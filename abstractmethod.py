e09a9c1a2eed210d7ec3e56ee0da97103b4b9e23
bohdilong <98560374+bohdilong@users.noreply.github.com>
2022-03-31 14:41:02 -0700

    Update abstractmethod.py

--- abstractmethod.py
+++ abstractmethod.py
@@ -1,12 +1,17 @@
 from abc import ABC, abstractmethod
+
+
 class car(ABC):
+    #this function prints how much your purchase was
     def paySlip(self, amount):
         print('your purchase amount: ',amount)
+    #this function is asking to pass in a an aurgument    
     @abstractmethod
     def payment(self, amount):
         pass
 
 class DebtCardPayment(car):
+    
     def payment(self, amount):
         print('Your purchase amount of {} exceeded your $100 limit '.format(amount))
 
