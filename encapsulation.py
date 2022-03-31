a9fd53fbe749fe0fdf67a4a67015b63fdd3969cc
bohdilong <98560374+bohdilong@users.noreply.github.com>
2022-03-31 10:25:25 -0700

    Update encapsulation.py

--- encapsulation.py
+++ encapsulation.py
@@ -1,15 +1,16 @@
 
 # class to store methods to restrict other methods and variables from accedentlly altering the stored code
 class Protected:
+    # method containing a private variable
     def __init__(self):
         self.__privateVar = 12
-
+    # method that prints variable
     def getPrivate(self):
         print(self.__privateVar)
-
+    # asigning the private variable the tag 'private'
     def setPrivate(self, private):
         self.__privateVar = private
-
+# calling class and its containing methods
 obj = Protected()
 obj.getPrivate()
 obj.setPrivate(23)
